5987886d3c5057551b965331b66c49566d65cc8d
Jesse <jessebot.git@gmail.com>
2025-07-21 16:02:42 -0700

    fix: properly create a new copy of the target file

--- main.go
+++ main.go
@@ -9,27 +9,38 @@ import (
 
 func main() {
 	saveDir := ".save"
-
 	if err := os.MkdirAll(saveDir, 0755); err != nil {
 		errExit(1, err.Error())
 	}
 
-	t := time.Now()
-	formattedDate := t.Format("20060102T150405")
-
+	currentTime := time.Now()
+	formattedTime := currentTime.Format("20060102T150405")
 	targetPath := filepath.Join("Draft.md")
 	targetExtension := filepath.Ext(targetPath)
-	newFileBaseName := formattedDate + "--" + "draft" + targetExtension
+	newFileBaseName := formattedTime + "--" + "draft" + targetExtension
 	newFilePath := filepath.Join(saveDir, newFileBaseName)
 
-	if err := os.Link(targetPath, newFilePath); err  != nil {
+	body, err := os.ReadFile(targetPath)
+	if err != nil {
 		errExit(1, err.Error())
 	}
+
+	f, err := os.Create(newFilePath)
+	if err != nil {
+		errExit(1, err.Error())
+	}
+	defer f.Close()
+
+	n, err := f.Write(body)
+	if err != nil {
+		errExit(1, err.Error())
+	}
+	_ = n
 
 	fmt.Println(newFileBaseName)
 }
 
 func errExit(status int, format string, a ...any) {
-	fmt.Fprintf(os.Stderr, "save error: " + format + "\n", a...)
+	fmt.Fprintf(os.Stderr, "save error: "+format+"\n", a...)
 	os.Exit(status)
 }
